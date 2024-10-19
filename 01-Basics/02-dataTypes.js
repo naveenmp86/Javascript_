be1057f88828c3fd7debe71ce77af191c2b8ca18
naveenmp86 <naveenmp19@gmail.com>
2024-10-19 19:12:00 +0530

    Data Types

---
+++ 01-Basics/02-dataTypes.js
@@ -0,0 +1,25 @@
+"use strict"     //Treat all JS code as newer Version
+
+//alert(3+3)  we are using node.js ,not browser
+
+console.log(3+3)   //Code readability must be high
+
+console.log("Naveen");
+
+let name = "Naveen";
+let age = 20;
+let isLoggedIn = true;
+
+
+
+// number  -->  2 to power 53
+// bigint
+// string  -->  ""
+//boolean  -->  true/false
+//null
+//undefined  -->
+
+
+console.log(typeof "Naveen");      //string
+console.log(typeof undefined)     //undefined
+console.log(typeof null)         //object
