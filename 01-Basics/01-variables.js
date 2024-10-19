47eb13e61e35d2e1c3aef388a84067292717c51b
naveenmp86 <naveenmp19@gmail.com>
2024-10-19 12:43:34 +0530

    Container config done

---
+++ 01-Basics/01-variables.js
@@ -0,0 +1,8 @@
+const accountId = 1234;
+let accountEmail = "Naveen@gmail.com";
+var accountPassword = "7654";
+accountCity = "Bengaluru";
+
+console.log(accountCity);
+
+console.table([accountId,accountEmail,accountPassword,accountCity]);
