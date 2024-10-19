5a6b87aeac4542e14cb8a51c205414be337ff94e
naveenmp86 <naveenmp19@gmail.com>
2024-10-19 20:02:29 +0530

    Type Conversion

--- 01-Basics/03-conversionOperator.js
+++ 01-Basics/03-conversionOperator.js
@@ -1,12 +1,14 @@
+/*************tc39 js documnetation ****************/
+
 let score = "33"
 
-console.log(typeof score);
-console.log(typeof(score))
+// console.log(typeof score);
+// console.log(typeof(score))
 
 let valueInNumber = Number(score);
 
-console.log(typeof valueInNumber)
-console.log(valueInNumber)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
 
 
 // "33" => 33
@@ -16,8 +18,55 @@ console.log(valueInNumber)
 let isLoggedIn = ""
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn)
+//console.log(booleanIsLoggedIn)
+
+
 
 // 1  => true
 // ""  => false
 //  "Naveen" => true
+
+
+
+let someNumber = 33
+
+let typeString = String(someNumber);
+// console.log("typeString")
+// console.log(typeof typeString)
+
+
+let value = 4
+let value1 = - value
+//console.log(value1)
+
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/2);
+// console.log(2%2);
+
+let str1 = "Hello";
+let str2 =" Naveen";
+
+let str3 = str1 +str2;
+//console.log(str3)
+
+
+// console.log("1" + 2)
+// console.log(1 + "2")
+// console.log("1" + 2 + 2)
+// console.log(1 + 2 + "2")
+
+// console.log( (2 + 3) * 4 / 2)
+
+
+// console.log(true)
+// console.log(+true)
+// console.log(+"")
+
+
+
+//link to study
+//https://tc39.es/ecma262/#sec-type-conversion
