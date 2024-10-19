8679887f295035d95f6c1b57b5a3f5bf249a3f9b
naveenmp86 <naveenmp19@gmail.com>
2024-10-19 23:28:01 +0530

    Comparision Operator

---
+++ 01-Basics/04-comparision.js
@@ -0,0 +1,22 @@
+// console.log(2 > 1)
+// console.log(2 >= 1)
+// console.log(2 < 1)
+// console.log(2 == 1)
+// console.log(2 != 1)
+
+// console.log("2" > 1)
+// console.log("2" == 1)
+
+console.log(null > 0)
+console.log(null == 0)
+console.log(null >= 0) 
+
+//comparision convert null to a number,treating it to 0
+
+console.log(undefined > 0)
+console.log(undefined == 0)
+console.log(undefined >= 0)
+
+// === "strict comaparision"
+
+console.log("2" === 2)
