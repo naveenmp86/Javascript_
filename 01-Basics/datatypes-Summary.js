410056f75415c0636872883d0f86051a0e3e5f51
naveenmp86 <naveenmp19@gmail.com>
2024-10-20 12:15:20 +0530

    dataType Summary

--- 01-Basics/datatypes-Summary.js
+++ 01-Basics/datatypes-Summary.js
@@ -42,3 +42,24 @@ console.log(typeof bigNumber)
 
 // link to study
 // https://262.ecma-international.org/5.1/#sec-11.4.3
+
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// Stack Memory (Primitive) ----> copy
+// Heap Memory (Non Primitive) ----> reference
+
+let myName = "Naveen";
+let myNewName = "Krishna";
+
+console.log(myNewName)
+
+let user1 = {
+    Email : "abc@gmail.com",
+}
+
+let user2 = user1;
+
+ user2.Email = "naveen@gmail.com"
+console.log(user1)
+console.log(user2)
