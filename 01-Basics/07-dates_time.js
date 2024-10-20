74a46fa24b8b154fcfea379455ede61bb7d391e2
naveenmp86 <naveenmp19@gmail.com>
2024-10-20 19:54:27 +0530

    Date and Time info

---
+++ 01-Basics/07-dates_time.js
@@ -0,0 +1,24 @@
+//console.log(Date());
+
+// let myDate = new Date();
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toTimeString())
+// console.log(typeof myDate)
+
+
+let myCreatedDate = new Date(2020 , 8 , 19)
+// console.log(myCreatedDate.toString())
+// console.log(myCreatedDate.toLocaleDateString())
+
+let myCreatedDate1 = new Date("09-19-2004")
+//console.log(myCreatedDate1.toLocaleDateString())
+
+let myStamp = Date.now();
+//console.log(myStamp)
+
+let newDate = new Date()
+console.log(newDate.getFullYear())
+console.log(newDate.getMonth())
+console.log(newDate.getDay())
+console.log(newDate.getHours())
