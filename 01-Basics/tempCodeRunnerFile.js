41496af875c0c0e8c0c7c9074bab0860ecb783f7
naveenmp86 <naveenmp19@gmail.com>
2024-12-01 12:20:27 +0530

    .

---
+++ 01-Basics/tempCodeRunnerFile.js
@@ -0,0 +1,7 @@
+console.log(Math)
+// console.log(Math.abs(-4));                          //Returns positive value
+// console.log(Math.round(96.8));                     //Returns round off value
+// console.log(Math.max(24,48,96,78,84));            //Returns max value among set
+// console.log(Math.min(11,8,7,-4,6,18));           //Returns min value among set
+// console.log(Math.floor(8.85));                  //Returns the greatest integer less than or equal to its numeric value
+// console.log(Math.ceil(8.85));     
