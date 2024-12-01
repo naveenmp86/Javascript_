3b125d1d5395f08bc8a8e026d471d37a1dcf15f2
naveenmp86 <naveenmp19@gmail.com>
2024-12-01 12:02:42 +0530

    for IN loop

---
+++ 05-Iterators/for_in.js
@@ -0,0 +1,20 @@
+const myObject = {
+    js : "javascript",
+    cpp : "C++",
+    py : "Python",
+    rb : "Ruby"
+}
+
+for (const key in myObject) {
+   //console.log(`${key} is shortcut for ${myObject[key]}`)
+}
+
+// const map = new Map()
+// map.set("IN","India")
+// map.set("EN","England")
+// map.set("SA","SouthAfrica")
+// map.set("NZ","Newzland")
+
+// for (const key in map) {
+//     console.log(key)
+// }
