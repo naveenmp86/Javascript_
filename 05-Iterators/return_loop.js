330eb7a43f7c2f7d7b8cf654b57d5e960ec87f7c
naveenmp86 <naveenmp19@gmail.com>
2024-12-01 12:03:11 +0530

    return loop

---
+++ 05-Iterators/return_loop.js
@@ -0,0 +1,15 @@
+const coding = ["js" , "cpp" , "py" , "rb" , "java"]
+
+// const values = coding.forEach( (item) => {
+//     console.log(item);
+//     return item;
+// })
+
+// console.log(values)
+
+
+const myNums = [1,2,3,4,5,6,7,8,9,10]
+
+let newNums = myNums.filter( (nums) => nums > 5)
+console.log(newNums);
+
