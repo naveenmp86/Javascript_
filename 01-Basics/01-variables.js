5f868e4af1c0fc58c222e54cad76eb5a0b170b37
Naveen M Padiyappanavar <162181189+naveenmp86@users.noreply.github.com>
2025-04-03 17:23:26 +0530

    Update 01-variables.js

--- 01-Basics/01-variables.js
+++ 01-Basics/01-variables.js
@@ -6,6 +6,12 @@ accountCity = "Bengaluru";
 console.log(accountCity);
 
 /*
+var: Declares a variable that can be used anywhere in a function or globally, and you can re-declare it.
+
+let: Declares a variable that can only be used inside the block (like inside a loop), and you can’t re-declare it in the same place.
+*/
+
+/*
 Prefer not to use var 
 because of issue in block scope and function scope
 */
