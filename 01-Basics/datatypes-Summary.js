146a9cb179ea3e6ed4ac7c7d53699226a772de61
Naveen M Padiyappanavar <162181189+naveenmp86@users.noreply.github.com>
2025-04-03 18:25:20 +0530

    Update datatypes-Summary.js

--- 01-Basics/datatypes-Summary.js
+++ 01-Basics/datatypes-Summary.js
@@ -19,14 +19,9 @@ const anotherId = Symbol('123')
 
 const bigNumber = 369258147852369n
 
-
-
-
 // Reference (Non-Primitive)
-
 // Arrays , Objects , Functions
  
-
 const myHeros = ["Naveen","Santosh","Manju"];
 console.table(myHeros)
 
@@ -62,6 +57,10 @@ let user1 = {
 
 let user2 = user1;
 
- user2.Email = "naveen@gmail.com"
+user2.Email = "naveen@gmail.com"
 console.log(user1)
 console.log(user2)
+
+// Here, you're not creating a new copy of user1 for user2. Instead, you're making user2 reference the same object as user1.
+
+// In JavaScript, objects are reference types. When you assign user1 to user2, both user1 and user2 point to the same object in memory.
